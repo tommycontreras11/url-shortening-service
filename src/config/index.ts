ed7194754334f825320372ebef951c91826432c0
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-09-01 21:10:36 -0400

    fix: update DB_PASSWORD retrieval to use process.env directly

--- src/config/index.ts
+++ src/config/index.ts
@@ -20,5 +20,5 @@ export default {
     DB_PORT: Number(required("DB_PORT")),
     DB_NAME: required("DB_NAME"),
     DB_USER: required("DB_USER"),
-    DB_PASSWORD: required("DB_PASSWORD")
+    DB_PASSWORD: process.env["DB_PASSWORD"]
 }
