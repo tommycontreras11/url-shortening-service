beef20a30a5352b44edef661c34f905c52c069ec
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-09-04 17:44:18 -0400

    refactor: update response data handling in short URL controllers

--- backend/src/modules/short-url/short-url.controller.ts
+++ backend/src/modules/short-url/short-url.controller.ts
@@ -8,8 +8,8 @@ export const getShortUrlByShortCodeController = async (
 ) => {
   const { shortCode } = req.params as { shortCode: string };
 
-  const data = await shortUrlService.getByShortCode(shortCode);
-  return res.status(StatusCode.OK).json({ data });
+  const shortUrl = await shortUrlService.getByShortCode(shortCode);
+  return res.status(StatusCode.OK).json({ data: shortUrl });
 };
 
 export const getShortUrlStatsByShortCodeController = async (
@@ -18,20 +18,20 @@ export const getShortUrlStatsByShortCodeController = async (
 ) => {
   const { shortCode } = req.params as { shortCode: string };
 
-  const data = await shortUrlService.getStatsByShortCode(shortCode);
-  return res.status(StatusCode.OK).json({ data });
+  const shortUrl = await shortUrlService.getStatsByShortCode(shortCode);
+  return res.status(StatusCode.OK).json({ data: shortUrl });
 };
 
 export const createShortUrlController = async (req: Request, res: Response) => {
-  const data = await shortUrlService.create(req.body);
-  return res.status(StatusCode.CREATED).json({ data });
+  const shortUrl = await shortUrlService.create(req.body);
+  return res.status(StatusCode.CREATED).json({ data: shortUrl });
 };
 
 export const updateShortUrlController = async (req: Request, res: Response) => {
   const { shortCode } = req.params as { shortCode: string };
 
-  const data = await shortUrlService.update(shortCode, req.body.url);
-  return res.status(StatusCode.OK).json({ data });
+  const shortUrl = await shortUrlService.update(shortCode, req.body.url);
+  return res.status(StatusCode.OK).json({ data: shortUrl });
 };
 
 export const deleteShortUrlController = async (req: Request, res: Response) => {
