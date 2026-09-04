a5696b9634444cdf2a0a5e820896371cb6c5095d
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-09-04 17:44:11 -0400

    feat: add CORS support and update package dependencies for backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,13 @@ import "reflect-metadata"
 import express from "express"
 import routes from "./routes/index.js"
 import { errorHandler } from "./middlewares/error-handler.middleware.js"
+import cors from "cors"
 
 const app = express()
 
+app.use(cors({
+    origin: "http://localhost:3001"
+}))
 app.use(express.json())
 app.use(routes)
 app.use(errorHandler)
